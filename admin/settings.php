8df0c7774ef22eead51631558e94e35da86057fd
HuzaifaaM007 <huzaifamurtaza007@gmail.com>
2026-09-02 17:26:50 +0500

    Update plugin admin settings page

--- admin/settings.php
+++ admin/settings.php
@@ -12,6 +12,7 @@
 require_once plugin_dir_path(__FILE__) . '../includes/wcgpq_forms_templates.php';
 
 
+
 add_action('woocommerce_admin_field_wcgpq_preview', 'wcgpq_render_preview_fields', 10);
 error_log("=== HOOK REGISTERED: woocommerce_admin_field_wcgpq_preview ===");
 
@@ -37,221 +38,162 @@ function wcgpq_render_preview_fields($value)
 function wcgpq_admin_settings()
 {
 
+    $config = require_once plugin_dir_path(__FILE__) . '../includes/wcgpq_config.php';
     // settings code
-    $wcgpq_settings_array = array();
+    $wcgpq_settings_array   =  array();
 
     $wcgpq_settings_array[] =  array(
-        'title' => __('WooCommerce Get Product Quote', 'wcgpq'),
-        'type' => 'title',
-        'desc' => __('Adds the gets qoute option on products ', 'wcgpq'),
-        'id' => 'wcgpq_settings_title'
+        'title'             =>  __('WooCommerce Get Product Quote', 'wcgpq'),
+        'type'              =>  'title',
+        'desc'              =>  __('Adds the gets qoute option on products ', 'wcgpq'),
+        'id'                =>  'wcgpq_settings_title'
     );
-    // array(
-    //     'title' => __('Get product quote listing page', 'wcgpq'),
-    //     'desc' => __('Add get product quote in listing page for each product', 'wcgpq'),
-    //     // 'id' => 'wcgpq_locations[product_list]',
-    //     'id' => 'wcgpq_location_product_list',
-    //     'type' => 'checkbox',
-    //     'default' => 'no'
-
-    // ),
-    // array(
-    //     'title' => __('Get product quote details page', 'wcgpq'),
-    //     'desc' => __('Add get product quote in details page of each product', 'wcgpq'),
-    //     // 'id' => 'wcgpq_locations[product_details]'
-    //     'id' => 'wcgpq_location_product_details',
-    //     'type' => 'checkbox',
-    //     'default' => 'no'
-
-    // ),
 
     $wcgpq_settings_array[] =   array(
-        'title' => __('Get product quote cart page', 'wcgpq'),
-        'desc' => __('Add get product quote in shop page for each product', 'wcgpq'),
-        // 'id' => 'wcgpq_locations[product_list]',
-        'id' => 'wcgpq_location_product_list',
-        'type' => 'checkbox',
-        'default' => 'no'
+        'title'             =>  __('Get product quote cart page', 'wcgpq'),
+        'desc'              =>  __('Add get product quote in shop page for each product', 'wcgpq'),
+        'id'                =>  'wcgpq_location_product_list',
+        'type'              =>  'checkbox',
+        'default'           =>  'no'
 
     );
 
     $wcgpq_settings_array[] =   array(
-        'title' => __('Get product quote cart page', 'wcgpq'),
-        'desc' => __('Add get product quote in product details page', 'wcgpq'),
-        // 'id' => 'wcgpq_locations[product_list]',
-        'id' => 'wcgpq_location_product_details',
-        'type' => 'checkbox',
-        'default' => 'no'
+        'title'             =>  __('Get product quote cart page', 'wcgpq'),
+        'desc'              =>  __('Add get product quote in product details page', 'wcgpq'),
+        'id'                =>  'wcgpq_location_product_details',
+        'type'              =>  'checkbox',
+        'default'           =>  'no'
 
     );
 
     $wcgpq_settings_array[] =   array(
-        'title' => __('Get product quote cart page', 'wcgpq'),
-        'desc' => __('Add get product quote in listing page for all products in cart', 'wcgpq'),
-        // 'id' => 'wcgpq_locations[product_list]',
-        'id' => 'wcgpq_location_cart',
-        'type' => 'checkbox',
-        'default' => 'no'
+        'title'             =>  __('Get product quote cart page', 'wcgpq'),
+        'desc'              =>  __('Add get product quote in listing page for all products in cart', 'wcgpq'),
+        'id'                =>  'wcgpq_location_cart',
+        'type'              =>  'checkbox',
+        'default'           =>  'no'
 
     );
 
     $wcgpq_settings_array[] = array(
-        'title' => __('Get product quote form', 'wcgpq'),
-        'desc' => __('Select the type of the form to display for getting product quote', 'wcgpq'),
-        'id' => 'wcgpq_form_type',
-        'type' => 'select',
-        'options' => wcgpq_get_form_choices(),
-        'default' => 'form_a',
-        'desc_tip' => true,
+        'title'             =>  __('Get product quote form', 'wcgpq'),
+        'desc'              =>  __('Select the type of the form to display for getting product quote', 'wcgpq'),
+        'id'                =>  'wcgpq_form_type',
+        'type'              =>  'select',
+        'options'           =>  wcgpq_get_form_choices(),
+        'default'           =>  'form_a',
+        'desc_tip'          =>  true,
 
     );
 
     $wcgpq_settings_array[] = array(
-        'type' => 'sectionend',
-        'id' => 'wcgpq_form_section_end'
+        'title'             =>  'sectionend',
+        'id'                =>  'wcgpq_form_section_end'
     );
 
     $wcgpq_settings_array[] = array(
-        'title' => 'Form Preview',
-        'type' => 'title',
-        'desc' => 'Below is how your selected form will look',
-        'id' => 'wcgpq_form_preview_header',
-        'desc_tip' => true
+        'title'             =>  'Form Preview',
+        'type'              =>  'title',
+        'desc'              =>  'Below is how your selected form will look',
+        'id'                =>  'wcgpq_form_preview_header',
+        'desc_tip'          =>  true
     );
 
-    error_log("Adding wcgpq_preview field type to settings array");
-
-
     $wcgpq_settings_array[] = array(
-        'type' => 'wcgpq_preview',
-        'id' => 'wcgpq_preview',
-        /**removed these as in description wp_sanitization remove the input fields of the form*/
-        // 'desc' => wcgpq_generate_form_preview('form_a')
-        'form_id' => 'form_a'
-
-    );
-
-    error_log("Form A preview added");
-
-    // $wcgpq_settings_array[] = array(
-    //     'type' => 'sectionend',
-    //     'id' => 'wcgpq_preview_form_a_end'
-
-    // );
-
-    $wcgpq_settings_array[] = array(
-        'type' => 'wcgpq_preview',
-        'id' => 'wcgpq_preview',
-        // 'desc' => wcgpq_generate_form_preview('form_b')
-        'form_id' => 'form_b'
-    );
-
-    error_log("Form B preview added");
-
-    // $wcgpq_settings_array[] = array(
-    //     'type' => 'sectionend',
-    //     'id' => 'wcgpq_preview_form_b_end'
-
-    // );
-
-    // array(
-    //     'type' => 'title',
-    //     'id' => 'wcgpq_form-1_preview',
-    //     'desc' => '
-    //         <div class="wcgpq-form-preview wcgpq-form-1">
-    //             <h3>Form A Preview</h3>
-    //             <p><input type="text" placeholder="Username" disabled></p>
-    //             <p><input type="email" placeholder="Email" disabled></p>
-    //             <p><input type="text" placeholder="Phone" disabled></p>
-    //             <p><input type="text" placeholder="Product" disabled></p>
-    //         </div>',
-    // ),
-    // array(
-    //     'type' => 'sectioned',
-    //     'id' => 'wcgpq_form_1_preview_end'
-    // ),
-
-    $wcgpq_settings_array[] = array(
-        'title' => __('Mailing Host', 'wcgpq'),
-        'desc' => __('Enter the hosting service', 'wcgpq'),
-        'id' => 'wcgpq_mailing_service',
-        'type' => 'text',
-        'default' => 'smtp.gmail.com',
-        'desc_tip' => true
+        'title'             =>  'wcgpq_preview',
+        'id'                =>  'wcgpq_preview',
+        'form_id'           =>  'form_a'
 
     );
 
     $wcgpq_settings_array[] = array(
-        'title' => __('Mailer Username', 'wcgpq'),
-        'desc' => __('Enter the mailing service user name', 'wcgpq'),
-        'id' => 'wcgpq_mailer_username',
-        'type' => 'text',
-        'default' => '',
-        'desc_tip' => true
+        'title'             =>  'wcgpq_preview',
+        'id'                =>  'wcgpq_preview',
+        'form_id'           =>  'form_b'
     );
 
     $wcgpq_settings_array[] = array(
-        'title' => __('Mailer Password', 'wcgpq'),
-        'desc' => __('Enter the 16 digit password issued for the app', 'wcgpq'),
-        'id' => 'wcgpq_mailer_password',
-        'type' => 'password',
-        'default' => '',
-        'desc_tip' => true
+        'title'             =>  __('Mailing Host', 'wcgpq'),
+        'desc'              =>  __('Enter the hosting service', 'wcgpq'),
+        'id'                =>  'wcgpq_mailing_service',
+        'type'              =>  'text',
+        'default'           => 'smtp.gmail.com',
+        'desc_tip'          => true
+
     );
 
     $wcgpq_settings_array[] = array(
-        'title' => __('Mailer From', 'wcgpq'),
-        'desc' => __('Enter the sender email', 'wcgpq'),
-        'id' => 'wcgpq_sender_mail',
-        'type' => 'text',
-        'default' => '',
-        'desc_tip' => true
+        'title'             =>  __('Mailer Username', 'wcgpq'),
+        'desc'              =>  __('Enter the mailing service user name', 'wcgpq'),
+        'id'                =>  'wcgpq_mailer_username',
+        'type'              =>  'text',
+        'default'           =>  '',
+        'desc_tip'          =>  true
     );
 
     $wcgpq_settings_array[] = array(
-        'title' => __('Admin Email', 'wcgpq'),
-        'desc' => __('Enter email here', 'wcgpq'),
-        'id' => 'wcgpq_admin_email',
-        'type' => 'text',
-        'default' => '',
-        'desc_tip' => true
+        'title'             =>  __('Mailer Password', 'wcgpq'),
+        'desc'              =>  __('Enter the 16 digit password issued for the app', 'wcgpq'),
+        'id'                =>  'wcgpq_mailer_password',
+        'type'              =>  'password',
+        'default'           =>  '',
+        'desc_tip'          =>  true
     );
 
     $wcgpq_settings_array[] = array(
-        'title' => __('Email Subject', 'wcgpq'),
-        'id' => 'wcgpq_email_subject',
-        'type' => 'text',
-        'default' => 'Product Quotation',
-        'desc_tip' => true,
-        'description' => 'Subject for Product Quotation'
+        'title'             =>  __('Mailer From', 'wcgpq'),
+        'desc'              =>  __('Enter the sender email', 'wcgpq'),
+        'id'                =>  'wcgpq_sender_mail',
+        'type'              =>  'text',
+        'default'           =>  '',
+        'desc_tip'          =>  true
     );
 
     $wcgpq_settings_array[] = array(
-        'title' => __('Email Template for Quote from Cart', 'wcgpq'),
-        'id' => 'wcgpq_email_template_for_cart_quote',
-        'type' => 'textarea',
-        'css' => 'min-width:400px; min-height:150px',
-        'default' => "New Quote Request Received\n\nA customer has requested a quote for their cart. Here are the details:\n\nCustomer Name: {name}\nCustomer Email: {email}\nPhone: {phone}\nCompany: {company}\n\nRequested Items:\n{cart_items}\n\nTotal Quantity: {total_quantity}\n\nCustomer Message:\n{message}\n\nYou can review and manage products from your admin dashboard using the links above.\n\nRegards,\n{store_name}",
-        'desc_tip' => true,
-        'description' => 'use placeholders like {name} ,{email}, {comments}'
+        'title'             =>  __('Admin Email', 'wcgpq'),
+        'desc'              =>  __('Enter email here', 'wcgpq'),
+        'id'                =>  'wcgpq_admin_email',
+        'type'              =>  'text',
+        'default'           =>  '',
+        'desc_tip'          =>  true
     );
 
     $wcgpq_settings_array[] = array(
-        'title' => __('Email Template for single Product Quote', 'wcgpq'),
-        'id' => 'wcgpq_email_template_for_product_quote',
-        'type' => 'textarea',
-        'css' => 'min-width:400px; min-height:150px',
-        'default' => "New Quote Request Received\n\nA customer has requested a quote for their cart. Here are the details:\n\nCustomer Name: {name}\nCustomer Email: {email}\nPhone: {phone}\nCompany: {company}\n\nRequested Items:\n{cart_items}\n\nTotal Quantity: {total_quantity}\n\nCustomer Message:\n{message}\n\nYou can review and manage products from your admin dashboard using the links above.\n\nRegards,\n{store_name}",
-        'desc_tip' => true,
-        'description' => 'use placeholders like {name} ,{email}, {comments}'
+        'title'             =>  __('Email Subject', 'wcgpq'),
+        'id'                =>  'wcgpq_email_subject',
+        'type'              =>  'text',
+        'default'           =>  'Product Quotation',
+        'desc_tip'          =>  true,
+        'description'       =>  'Subject for Product Quotation'
     );
 
     $wcgpq_settings_array[] = array(
-        'type' => 'sectionend',
-        'id' => 'wcgpq_email_settings_title'
+        'title'             =>  __('Email Template for Quote from Cart', 'wcgpq'),
+        'id'                =>  'wcgpq_email_template_for_cart_quote',
+        'type'              =>  'textarea',
+        'css'               =>  'min-width: 600px; height: 300px; font-family: monospace;',
+        'default'           =>  $config['default_cart_template'],
+        'sanitize_callback' => 'wp_kses_post',
+        'desc_tip'          => true,
+        'description'       => 'use placeholders like {name} ,{email}, {comments}'
     );
 
+    $wcgpq_settings_array[] = array(
+        'title'             =>  __('Email Template for single Product Quote', 'wcgpq'),
+        'id'                =>  'wcgpq_email_template_for_product_quote',
+        'type'              =>  'textarea',
+        'css'               =>  'min-width: 600px; height: 300px; font-family: monospace;',
+        'default'           =>  $config['default_product_template'],
+        'sanitize_callback' =>  'wp_kses_post',
+        'desc_tip'          =>  true,
+        'description'       =>  'use placeholders like {name} ,{email}, {comments}'
+    );
 
+    $wcgpq_settings_array[] = array(
+        'type'              => 'sectionend',
+        'id'                => 'wcgpq_email_settings_title'
+    );
 
     return $wcgpq_settings_array;
 }
@@ -300,20 +242,11 @@ add_action('woocommerce_settings_tabs_wcgpq_settings', 'wcgpq_settings_tab_conte
 function wcgpq_save_settings()
 {
     woocommerce_update_options(wcgpq_admin_settings());
-
-    // $locations = array(
-    // 'product_list' => get_option('wcgpq_location_product_list', 'no'),
-    // 'product_details' => get_option('wcgpq_location_product_details', 'no'),
-    //     'cart' => get_option('wcgpq_location_cart', 'no')
-    // );
-
-    // update_option('wcgpq_locations', $locations);
 }
 
 add_action('woocommerce_update_options_wcgpq_settings', 'wcgpq_save_settings');
 
-
-function wcgpq_admin_scripts($hook)
+function wcgpq_admin_scripts(string $hook)
 {
 
     if ($hook !== 'woocommerce_page_wc-settings') {
@@ -321,8 +254,6 @@ function wcgpq_admin_scripts($hook)
 
         return;
     }
-
-    error_log('wcgpq_admin_scripts');
 
     wp_enqueue_script(
         'wcgpq-admin-js',
